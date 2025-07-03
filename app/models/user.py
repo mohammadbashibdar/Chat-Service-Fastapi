36189426601c1dbef2b79b94b179fa3c9522ea08
Mohammad Bashibdar <bashibdar1384@gmail.com>
2025-07-03 11:55:23 +0330

    add table for User model.

---
+++ app/models/user.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Index, UniqueConstraint, Enum, TIMESTAMP, func
+from sqlalchemy.orm import relationship
+from app.schemas.enums import Gender
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    password = Column(String(255))
+    mobile_number = Column(String, nullable=False)
+    email = Column(String(255))
+    national_code = Column(String(255))
+    gender = Column(Enum(Gender), nullable=False)
+    profile_image = Column(String(36))
+    is_active = Column(Boolean, nullable=False)
+    loginable = Column(Boolean, nullable=False)
+    is_deleted = Column(Boolean, default=False)
+    mobile_visible = Column(Boolean, default=False)
+
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now(), nullable=True)
+
+
+    __table_args__ = (
+        UniqueConstraint('mobile_number', 'email', name='uniques'),
+        Index('index', 'name', 'mobile_number', 'email'),
+    )
